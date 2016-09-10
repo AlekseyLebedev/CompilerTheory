2105d55efdf5ea89b760d1b4307f8e0d938bca8a
Lebedev Aleksey <leb-al@outlook.com>
2016-09-10 22:23:44 +0300

    Класс операции

--- CompilerTheory/OpExp.h
+++ CompilerTheory/OpExp.h
@@ -1,17 +1,27 @@
 #pragma once
 
+#include "stdafx.h";
 #include "IExp.h";
 
 namespace FirstTask {
 	struct COpExp : public IExp {
-		IExp* LeftOperand;
-		IExp* RightOperand;
+	public:
 		enum TOperationType {
 			Plus,
 			Minus,
 			Times,
 			Divide
-		} operation;
+		};
+
+		COpExp(std::shared_ptr<IExp> left, std::shared_ptr<IExp> right, TOperationType type);
+		virtual ~COpExp();
+		std::shared_ptr<IExp> GetLeftOperand() const;
+		std::shared_ptr<IExp> GetRightOperand() const;
+		TOperationType GetOperationType() const;
+
+	private:
+		std::shared_ptr<IExp> leftOperand, rightOperand;
+		TOperationType operation;
 
 	};
 }
