dd7a3b6b0b7315163bf986fcd1e6266e0bc3bfad
Lebedev Aleksey <leb-al@outlook.com>
2016-09-18 00:52:43 +0300

    попытка перейти на С++

--- MiniJavaCompiler/MiniJavaCompiler.cpp
+++ MiniJavaCompiler/MiniJavaCompiler.cpp
@@ -1,14 +1,43 @@
 // MiniJavaCompiler.cpp: определяет точку входа для консольного приложения.
 //
 
-#include "stdafx.h"
-#include "..\winflexbison\tools\FlexLexer.h"
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <FlexLexer.h>
 
-int yylex();
+//int yylex();
+//FILE* yyin, *yyout;
 
-int main()
+int main(int argc, char** argv)
 {
-	yylex();
-    return 0;
+	if (argc == 0) {
+		//yylex();
+		yyFlexLexer lexer;
+		lexer.switch_streams(&std::cin, &std::cout);
+		lexer.yylex();
+	}
+	else {
+		std::stringstream buffer;
+		for (size_t i = 1; i < argc; i++)
+		{
+			buffer << argv[i];
+			std::cout << "Processing: " << buffer.str() << std::endl;
+			std::ifstream in(buffer.str());
+			buffer << "-out.txt";
+			std::cout << "Out path: " << buffer.str() << std::endl;
+			std::ofstream out(buffer.str().c_str());
+			yyFlexLexer lexer;
+			lexer.switch_streams(&in, &out);
+			lexer.yylex();
+			in.close();
+			out.flush();
+			out.close();
+			buffer.clear();
+		}
+	}
+	return 0;
 }
 
