3860685d8bb60a4efb9f4e5be820ee4dfaf6ca4f
Зятчин Валентин Михайлович <muxajlbl4.08@mail.ru>
2016-10-18 23:21:20 +0300

    add mooore classes

---
+++ AbstractTreeGenerator/VarDeclaration.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <memory>
+
+
+namespace FirstTask {
+	class CClassDeclaration {
+	public:
+		CClassDeclaration();
+		virtual ~CClassDeclaration();
+
+		//		virtual void Accept(IVisitor*) override;
+	private:
+	};
+}
