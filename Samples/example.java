ec5a3d971adfe6a43bbd5239c7ccb9a9e72a87fa
Sdernal <sdernal2@gmail.com>
2016-10-24 23:51:27 +0300

    added working example

---
+++ Samples/example.java
@@ -0,0 +1,16 @@
+class BinarySearch{
+    public static void main(String[] a){
+	System.out.println(a);
+    }
+}
+
+class BS {
+	int[] number ;
+    	int size ;
+public int Start(int sz){
+	int aux01;
+	int aux02;	
+	//d=0;
+return aux01;
+}
+}
