a501ddb9f403bfa1d646c3a146cbdd553435d42b
Зятчин Валентин Михайлович <muxajlbl4.08@mail.ru>
2016-11-10 01:00:48 +0300

    нужно реализовать все методы

--- MiniJavaCompiler/Table.cpp
+++ MiniJavaCompiler/Table.cpp
@@ -1,1 +1,40 @@
 #include "Table.h"
+#include "assert.h"
+
+void CTable::addBlock()
+{
+	
+}
+
+void CTable::deleteBlock()
+{
+	// проверка на то что в таблице еще есть блоки
+	assert( table.empty() );
+}
+
+void CTable::deleteBlock( block * theBlock )
+{
+}
+
+void CTable::insertSymbol( const CSymbol & theSymbol, const IInfo & theInfo )
+{
+	
+}
+
+void CTable::popSymbol( const std::string & theName )
+{
+}
+
+void CTable::popSymbol( const std::string & theName, const IInfo & theInfo )
+{
+}
+
+const IInfo CTable::lookup( const CSymbol & theSymbol ) const
+{
+	return IInfo();
+}
+
+const IInfo CTable::lookup( const std::string & theSymbolName ) const
+{
+	return IInfo();
+}
--- MiniJavaCompiler/Table.h
+++ MiniJavaCompiler/Table.h
@@ -1,21 +1,40 @@
 #pragma once
 
 #include <string>
-#include <unordered_map>
+#include <map>
+#include <list>
 #include "Symbol.h"
 #include "Info.h"
+
+typedef std::map< CSymbol, IInfo > block;
 
 // таблица символов
 class CTable {
 public:
-	void insert( const CSymbol& theSymbol, const IInfo& theInfo);
-	void pop(const std::string& theName );
+	// добавляет новый блок в таблицу
+	void addBlock();
+	// удаляет текущий блок
+	void deleteBlock();
+	// удаялет указанный блок
+	void deleteBlock( block* theBlock );
+
+	void insertSymbol( const CSymbol& theSymbol, const IInfo& theInfo );
+	void popSymbol( const std::string& theName );
 	// для избежания коллизий при удалении нужно передавать информацию об объекте
-	void pop(const std::string& theName, const IInfo& theInfo );
+	void popSymbol( const std::string& theName, const IInfo& theInfo );
+
+	// TODO: возвращать итератор
 	// метод для поиска переменной в таблице (включая все предыдущие секции)
-	const IInfo lookup(const CSymbol& theSymbol ) const;
+	const IInfo lookup( const CSymbol& theSymbol ) const;
+	// метод для поиска переменной в таблице по названию(включая все предыдущие секции)
+	const IInfo lookup( const std::string& theSymbolName ) const;
 
 private:
-	// не уверен что должно быть именно так
-	std::unordered_map < CSymbol, IInfo > table;
+	//Вариант реализации : список блоков
+	//При входе во вложенную область – создаем новый блок и помещаем переменные в него
+	//При выходе из области – удаляем последний блок
+	std::list< block > table;
+
+	// указатель на текущий блок
+	block* currentBlock;
 };
