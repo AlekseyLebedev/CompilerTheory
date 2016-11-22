95ead2c1491d18fb4ff27234c65796f02e76e3bd
Lebedev Aleksey <leb-al@outlook.com>
2016-11-22 16:27:47 +0300

    Пустой визитор для Type Checking

---
+++ MiniJavaCompiler/SymbolTable/TypeCheckerVistor.cpp
@@ -0,0 +1,154 @@
+#include "TypeCheckerVistor.h"
+
+namespace SymbolTable {
+
+	CTypeCheckerVistor::CTypeCheckerVistor()
+	{
+	}
+
+
+	CTypeCheckerVistor::~CTypeCheckerVistor()
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CArgument * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CArgumentList * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CAssignmentListStatement * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CAssignmentStatement * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CClassDeclaration * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CClassDeclarationList * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CClassExtend * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CCompoundStatement * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CConstructorExpression * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CExpressionList * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CIdExpression * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CIndexExpression * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CLastExpressionList * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CLengthExpression * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CListConstructorExpression * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CMainClass * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CMethodDeclaration * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CMethodDeclarationList * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CNegationExpression * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CNumberExpr * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::COperationExpression * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CParenExpression * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CPreconditionStatement * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CPrintStatement * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CProgram * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CStatementList * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CBasicType * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CIdType * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CVarDeclaration * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CVarDeclarationList * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CTrueExpression * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CFalseExpression * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CGetFieldExpression * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CConditionStatement * const )
+	{
+	}
+
+	void CTypeCheckerVistor::visit( AbstractTreeGenerator::CThisExpression * const )
+	{
+	}
+
+}
---
+++ MiniJavaCompiler/SymbolTable/TypeCheckerVistor.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "..\AbstractTreeGenerator\IVisitor.h"
+#include "..\AbstractTreeGenerator\AllNodes.h"
+
+namespace SymbolTable {
+	class CTypeCheckerVistor : public AbstractTreeGenerator::IVisitor {
+	public:
+		CTypeCheckerVistor();
+		virtual ~CTypeCheckerVistor();
+
+		virtual void visit( AbstractTreeGenerator::CArgument * const ) override;
+		virtual void visit( AbstractTreeGenerator::CArgumentList * const ) override;
+		virtual void visit( AbstractTreeGenerator::CAssignmentListStatement * const ) override;
+		virtual void visit( AbstractTreeGenerator::CAssignmentStatement * const ) override;
+		virtual void visit( AbstractTreeGenerator::CClassDeclaration * const ) override;
+		virtual void visit( AbstractTreeGenerator::CClassDeclarationList * const ) override;
+		virtual void visit( AbstractTreeGenerator::CClassExtend * const ) override;
+		virtual void visit( AbstractTreeGenerator::CCompoundStatement * const ) override;
+		virtual void visit( AbstractTreeGenerator::CConstructorExpression * const ) override;
+		virtual void visit( AbstractTreeGenerator::CExpressionList * const ) override;
+		virtual void visit( AbstractTreeGenerator::CIdExpression * const ) override;
+		virtual void visit( AbstractTreeGenerator::CIndexExpression * const ) override;
+		virtual void visit( AbstractTreeGenerator::CLastExpressionList * const ) override;
+		virtual void visit( AbstractTreeGenerator::CLengthExpression * const ) override;
+		virtual void visit( AbstractTreeGenerator::CListConstructorExpression * const ) override;
+		virtual void visit( AbstractTreeGenerator::CMainClass * const ) override;
+		virtual void visit( AbstractTreeGenerator::CMethodDeclaration * const ) override;
+		virtual void visit( AbstractTreeGenerator::CMethodDeclarationList * const ) override;
+		virtual void visit( AbstractTreeGenerator::CNegationExpression * const ) override;
+		virtual void visit( AbstractTreeGenerator::CNumberExpr * const ) override;
+		virtual void visit( AbstractTreeGenerator::COperationExpression * const ) override;
+		virtual void visit( AbstractTreeGenerator::CParenExpression * const ) override;
+		virtual void visit( AbstractTreeGenerator::CPreconditionStatement * const ) override;
+		virtual void visit( AbstractTreeGenerator::CPrintStatement * const ) override;
+		virtual void visit( AbstractTreeGenerator::CProgram * const ) override;
+		virtual void visit( AbstractTreeGenerator::CStatementList * const ) override;
+		virtual void visit( AbstractTreeGenerator::CBasicType * const ) override;
+		virtual void visit( AbstractTreeGenerator::CIdType * const ) override;
+		virtual void visit( AbstractTreeGenerator::CVarDeclaration * const ) override;
+		virtual void visit( AbstractTreeGenerator::CVarDeclarationList * const ) override;
+		virtual void visit( AbstractTreeGenerator::CTrueExpression * const ) override;
+		virtual void visit( AbstractTreeGenerator::CFalseExpression * const ) override;
+		virtual void visit( AbstractTreeGenerator::CGetFieldExpression * const ) override;
+		virtual void visit( AbstractTreeGenerator::CConditionStatement * const ) override;
+		virtual void visit( AbstractTreeGenerator::CThisExpression * const ) override;
+	};
+
+}
