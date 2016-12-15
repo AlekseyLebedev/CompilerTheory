31b4384e5692ed490ff6317cf9bd76bb4b45f3dc
volodden <volodin@phystech.edu>
2016-12-15 12:49:03 +0300

    Expression Converter.

---
+++ MiniJavaCompiler/IRTree/IRTExpConverter.cpp
@@ -0,0 +1,20 @@
+#include "IRTExpConverter.h"
+
+Translate::IRTExpConverter::IRTExpConverter( const IRTree::IRTExpression* _exp ) : exp( _exp )
+{
+}
+
+const IRTree::IRTExpression* ToExpression() const override
+{
+
+}
+
+const IRTree::IRTStatement* ToStatement() const override
+{
+
+}
+
+const IRTree::IRTStatement* ToConditional() const override
+{
+
+}
