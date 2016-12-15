e5003c8db4c35dc6c55a59b749931b057f0894e9
volodden <volodin@phystech.edu>
2016-12-15 05:04:58 +0300

    Fix heder.

--- MiniJavaCompiler/IRTree/IRTreeClasses.h
+++ MiniJavaCompiler/IRTree/IRTreeClasses.h
@@ -40,3 +40,19 @@ namespace IRTree {
 
 #include "IRTLabel.h"
 #include "IRTTemp.h"
+
+#include "ExpressionClasses\IRTEConst.h"
+#include "ExpressionClasses\IRTEName.h"
+#include "ExpressionClasses\IRTETemp.h"
+#include "ExpressionClasses\IRTEBinop.h"
+#include "ExpressionClasses\IRTEMem.h"
+#include "ExpressionClasses\IRTECall.h"
+#include "ExpressionClasses\IRTEEseq.h"
+
+#include "StatementClasses\IRTSMove.h"
+#include "StatementClasses\IRTSMove.h"
+#include "StatementClasses\IRTSExp.h"
+#include "StatementClasses\IRTSJump.h"
+#include "StatementClasses\IRTSCjump.h"
+#include "StatementClasses\IRTSSeq.h"
+#include "StatementClasses\IRTSLabel.h"
