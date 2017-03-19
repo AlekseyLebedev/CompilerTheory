a9b9dab3f3b05f2e1bf64ff310ce28428943dc76
Зятчин Валентин Михайлович <muxajlbl4.08@mail.ru>
2017-03-19 23:07:36 +0300

    конструктор

--- MiniJavaCompiler/Generator.h
+++ MiniJavaCompiler/Generator.h
@@ -2,13 +2,21 @@
 #include <memory>
 #include <map>
 #include <list>
-#include "IRTree\StatementClasses\IRTSSeq.h"
+#include "IRTree\IRTreeAllClasses.h"
 
 using namespace IRTree;
 
 class CGenerator {
+public:
+	CGenerator( std::shared_ptr<IRTSSeq> theRoot ) : root ( theRoot ) {}
+	void SetRoot( std::shared_ptr<IRTSSeq> root );
+	std::shared_ptr<IRTSSeq> getRoot();
+
 
 private:
+	std::shared_ptr<IRTSSeq> root;
+
 	// храним список всех блоков в виде указателей на канонические деревья
 	std::list<std::shared_ptr<IRTSSeq> > basicBlocks;
+
 };
