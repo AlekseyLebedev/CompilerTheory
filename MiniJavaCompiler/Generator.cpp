7a01ef5a2d4cc4607a4df48af28615a050913cca
Зятчин Валентин Михайлович <muxajlbl4.08@mail.ru>
2017-03-19 23:20:22 +0300

    создаем кодогенератор

--- MiniJavaCompiler/Generator.cpp
+++ MiniJavaCompiler/Generator.cpp
@@ -1,2 +1,11 @@
 #include "Generator.h"
 
+void CGenerator::SetRoot( std::shared_ptr<IRTree::CCodeFragment>  theRoot )
+{
+	root = theRoot;
+}
+
+std::shared_ptr<IRTree::CCodeFragment>  CGenerator::GetRoot()
+{
+	return root;
+}
--- MiniJavaCompiler/Generator.h
+++ MiniJavaCompiler/Generator.h
@@ -3,18 +3,19 @@
 #include <map>
 #include <list>
 #include "IRTree\IRTreeAllClasses.h"
+#include "IRTree\CodeFragment.h"
 
 using namespace IRTree;
 
 class CGenerator {
 public:
-	CGenerator( std::shared_ptr<IRTSSeq> theRoot ) : root ( theRoot ) {}
-	void SetRoot( std::shared_ptr<IRTSSeq> root );
-	std::shared_ptr<IRTSSeq> getRoot();
+	CGenerator( std::shared_ptr<IRTree::CCodeFragment>  theRoot ) : root ( theRoot ) {}
+	void SetRoot( std::shared_ptr<IRTree::CCodeFragment>  theRoot );
+	std::shared_ptr<IRTree::CCodeFragment>  GetRoot();
 
 
 private:
-	std::shared_ptr<IRTSSeq> root;
+	std::shared_ptr<IRTree::CCodeFragment>  root;
 
 	// храним список всех блоков в виде указателей на канонические деревья
 	std::list<std::shared_ptr<IRTSSeq> > basicBlocks;
