ea1fc2c2c19b6cc46c57dd191242b7d689c976a4
Sdernal <sdernal2@gmail.com>
2017-03-26 19:52:35 +0300

    merge

--- MiniJavaCompiler/CodeGenerator/OperationType.cpp
+++ MiniJavaCompiler/CodeGenerator/OperationType.cpp
@@ -64,8 +64,6 @@ namespace CodeGeneration {
 				return L"XORI ' !";
 			case OT_CMP:
 				return L"CMP ' '";
-			case OT_CMPC:
-				return L"CMPC ' '";
 			case OT_JMP:
 				return L"JMP ^";
 			case OT_JE:
