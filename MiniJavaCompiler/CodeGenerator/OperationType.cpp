20909fb7f56913a3bd6ab2596cd4d5f8937d760d
Lebedev Aleksey <leb-al@outlook.com>
2017-03-26 15:41:37 +0300

    namespace CodeGeneration {

--- MiniJavaCompiler/CodeGenerator/OperationType.cpp
+++ MiniJavaCompiler/CodeGenerator/OperationType.cpp
@@ -2,40 +2,40 @@
 
 #include "OperationType.h"
 
-namespace CodeGeneartion {
+namespace CodeGeneration {
 
-std::wstring GetOperationString( TOperationType type )
-{
-	switch( type ) {
-		case OT_None:
-			assert( false );
-			break;
-		case OT_Move:
-			return L"MOVE ' '";
-		case OT_MemFramePointerPlusConst:
-			return L"MOVE ' [isp+!]";
-		case OT_AddTemps:
-			return L"ADD  ' ' '";
-		case OT_SubTemps:
-			return L"SUB  ' ' '";
-		case OT_MulTemps:
-			return L"MUL  ' ' '";
-		case OT_DivTemps:
-			return L"DIV  ' ' '";
-		case OT_AddTempConst:
-			return L"ADD  ' ' !";
-		case OT_SubTempConst:
-			return L"SUB  ' ' !";
-		case OT_MulTempConst:
-			return L"MUL  ' ' !";
-		case OT_DivTempConst:
-			return L"DIV  ' ' !";
-		case OT_LoadConst:
-			return L"LOAD ' !";
-		default:
-			assert( false );
-			break;
+	std::wstring GetOperationString( TOperationType type )
+	{
+		switch( type ) {
+			case OT_None:
+				assert( false );
+				break;
+			case OT_Move:
+				return L"MOVE ' '";
+			case OT_MemFramePointerPlusConst:
+				return L"MOVE ' [isp+!]";
+			case OT_AddTemps:
+				return L"ADD  ' ' '";
+			case OT_SubTemps:
+				return L"SUB  ' ' '";
+			case OT_MulTemps:
+				return L"MUL  ' ' '";
+			case OT_DivTemps:
+				return L"DIV  ' ' '";
+			case OT_AddTempConst:
+				return L"ADD  ' ' !";
+			case OT_SubTempConst:
+				return L"SUB  ' ' !";
+			case OT_MulTempConst:
+				return L"MUL  ' ' !";
+			case OT_DivTempConst:
+				return L"DIV  ' ' !";
+			case OT_LoadConst:
+				return L"LOAD ' !";
+			default:
+				assert( false );
+				break;
+		}
 	}
-}
 
 } // namespace CodeGeneartion
--- MiniJavaCompiler/CodeGenerator/OperationType.h
+++ MiniJavaCompiler/CodeGenerator/OperationType.h
@@ -2,25 +2,27 @@
 
 #include <string>
 
-enum TOperationType {
-	OT_None, // Не используется!
-	OT_Move, // Должно использоваться отдельно, для распределенияр регистров
-	OT_MemFramePointerPlusCont, // Обращеине к ISP + константа
-	OT_MemRegPlusConst, // Обращеине регистр + константа
-	OT_MemRegMinusConst, // Обращеине регистр - константа
-	OT_MemConstMinusReg, // Обращеине константа - регистр
-	OT_MemRegMulConst, // Обращеине регистр * константа
-	OT_AddConstToFirst, // Добавление к первому аргументу и т.д.
-	OT_AddTemps, // Бинарная операция сложения регистров
-	OT_SubTemps, // Бинарная операция вычитания регистров
-	OT_MulTemps, // Бинарная операция умножения регистров
-	OT_DivTemps, // Бинарная операция деления регистров
-	OT_AddTempConst, // Бинарная операция сложения регистра с константой
-	OT_SubTempConst, // Бинарная операция вычитания регистра с константой
-	OT_MulTempConst, // Бинарная операция умножения регистра с константой
-	OT_DivTempConst, // Бинарная операция деления регистра с константой
-	OT_LoadConst // Запоминаем константу в регистр
-};
+namespace CodeGeneration {
+
+	enum TOperationType {
+		OT_None, // Не используется!
+		OT_Move, // Должно использоваться отдельно, для распределенияр регистров
+		OT_MemFramePointerPlusConst, // Обращеине к ISP + константа
+		OT_MemRegPlusConst, // Обращеине регистр + константа
+		OT_MemRegMinusConst, // Обращеине регистр - константа
+		OT_MemConstMinusReg, // Обращеине константа - регистр
+		OT_MemRegMulConst, // Обращеине регистр * константа
+		OT_AddConstToFirst, // Добавление к первому аргументу и т.д.
+		OT_AddTemps, // Бинарная операция сложения регистров
+		OT_SubTemps, // Бинарная операция вычитания регистров
+		OT_MulTemps, // Бинарная операция умножения регистров
+		OT_DivTemps, // Бинарная операция деления регистров
+		OT_AddTempConst, // Бинарная операция сложения регистра с константой
+		OT_SubTempConst, // Бинарная операция вычитания регистра с константой
+		OT_MulTempConst, // Бинарная операция умножения регистра с константой
+		OT_DivTempConst, // Бинарная операция деления регистра с константой
+		OT_LoadConst // Запоминаем константу в регистр
+	};
 
 	std::wstring GetOperationString( TOperationType type );
 
